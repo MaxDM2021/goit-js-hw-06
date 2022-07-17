605a3b887196572abee965faf57c8938a4cae969
Maksym Dmitriiev <maximdm22@gmail.com>
2022-07-17 21:58:00 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const categoryEl = document.querySelectorAll('.item');
+const numberCategoryEl = categoryEl.length;
+console.log('Number of categories: ', numberCategoryEl);
+
+[...categoryEl].forEach(function (el) {
+    const numberOfElements = el.querySelectorAll('li').length;
+    const nameOfElemets = el.querySelector('h2').textContent;
+    console.log("Category: ", `${nameOfElemets}`);
+    console.log("Elements: ", `${numberOfElements}`);
+})
+
+
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulList = document.querySelector('#ingredients');
+console.log (ulList);
+
+const listM = ingredients.map(ingredient => {
+  const newLi = document.createElement('li');
+  newLi.textContent = ingredient;
+  newLi.classList.add('item');
+  return newLi;
+}).join("");
+
+ulList.append(listM)
+
+// [...ingredients].forEach
