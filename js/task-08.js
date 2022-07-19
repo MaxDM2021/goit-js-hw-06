d421439eb7408abd52e45014a8e5a211ac71eed9
Maksym Dmitriiev <maximdm22@gmail.com>
2022-07-19 21:13:31 +0300

    888

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
 
+const form = document.querySelector(".login-form");
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+event.preventDefault();
+const formElements = event.currentTarget.elements;
+const email = formElements.email.value;
+const password = formElements.password.value;
+
+const formData = { 
+    email: email,
+    password: password,
+}
+console.log(formData)
+form.reset();
+
+
+if(email === '' || password === '') {
+    alert('Пустые поля')
+    form.reset();
+    return;
+}
+}
+
+
+
