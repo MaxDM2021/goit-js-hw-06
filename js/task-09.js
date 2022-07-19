407c2988d9ea595efa23e1b71daa41dbd4a327f6
Maksym Dmitriiev <maximdm22@gmail.com>
2022-07-19 22:04:24 +0300

    999

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,26 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body')
+const colorEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color')
+
+
+buttonEl.addEventListener('click', onChangeColor);
+
+
+function onChangeColor() {
+  const changeColor = getRandomHexColor(); 
+body.style.backgroundColor = changeColor;
+colorEl.textContent = `${body.style.backgroundColor}`;
+}
+
+
+
+
+
+
+
